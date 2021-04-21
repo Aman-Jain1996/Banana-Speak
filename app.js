825bbd10d44812781d8193322d82732ea9a2e012
Aman Jain <ajain8479@gmail.com>
2021-04-21 15:35:52 +0530

    completed

--- app.js
+++ app.js
@@ -1,8 +1,9 @@
 var inputText = document.querySelector("#input-area");
 var btnTranslate = document.querySelector("#btn-translate");
 var outputArea = document.querySelector("#output-area");
+var btnReset =document.querySelector("#btn-reset");
 
-// var url = "https://First-API-Call.amanjain1996.repl.co/translate/yoda.json" ;
+//var url = "https://First-API-Call.amanjain1996.repl.co/translate/yoda.json" ;
 
 var url ="https://api.funtranslations.com/translate/minion.json";
 
@@ -11,17 +12,25 @@ function URLGenerator(text){
 }
 
 
+
 function APICall(){
     var URL=URLGenerator(inputText.value);
     fetch(URL)
     .then(response => response.json())
     .then(response =>{
-        outputArea.innerText=response.contents.translated;
+        outputArea.value=response.contents.translated;
         console.log(response);
     })
     .catch(() => { alert("Some server error");})
-
 }
 
 
+
 btnTranslate.addEventListener('click',APICall);
+btnReset.addEventListener('click',resetHandler);
+
+
+function resetHandler(){
+    inputText.value='';
+    outputArea.value='';
+}
